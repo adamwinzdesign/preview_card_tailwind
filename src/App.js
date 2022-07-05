b8810ea9b4e87ae758a5459bd34ef0c7fbeac00d
adamwinzdesign <adamwinzdesign@gmail.com>
2022-07-04 19:33:53 -0500

    updated card and tailwind.config

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ function App() {
 			{/* main */}
 			<div className='bg-cream h-screen w-screen flex items-center justify-center'>
 				{/* card */}
-				<div className='w-600 h-450 bg-white rounded-xl overflow-hidden flex'>
+				<div className='max-w-600 bg-white rounded-xl overflow-hidden flex'>
 					<div className='w-6/12'>
 						<img src={productDesktop} alt='product' />
 					</div>
 
-					<div className='w-6/12'>Parfum</div>
+					<div className='w-6/12'>
+						<div className='h-full p-8 flex flex-col items-left justify-center'>
+							<p className='font-sans text-xs text-aurometal_saurus font-medium uppercase'>
+								Perfume
+							</p>
+							<h1 className='font-bold text-big leading-8 font-serif text-gunmetal'>
+								Gabrielle Essence Eau De Parfum
+							</h1>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
 			current: "currentColor",
 			white: "#ffffff",
 			cream: "#F2EAE2",
+			aurometal_saurus: "#6C7289",
+			deep_aquamarine: "#3D8168",
+			deep_aquamarine_hover: "#1A4032",
+			gunmetal: "#1C232B",
+		},
+		fontFamily: {
+			sans: ["Montserrat", "sans-serif"],
+			serif: ["Fraunces", "serif"],
+		},
+		fontSize: {
+			big: "32px",
+		},
+		maxWidth: {
+			600: "600px",
 		},
 	},
 	plugins: [],
